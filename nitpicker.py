b76494161db7cfebbc7d0a473dca9d7eadde6e6e
Abdur Rehman <arehmanq199@gmail.com>
2016-12-18 05:43:37 +0500

    nitpicker.py: apply typo checking rules on a file
    Apply typo checking rules on the file pointed by arg1 on the cli.
    
    Signed-off-by: Abdur Rehman <arehmanq199@gmail.com>

--- nitpicker.py
+++ nitpicker.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import urllib.request
 import regex
+import sys
 
 
-def main():
+def main(argv):
 
     # Load the list of typos from the English Wikipedia
     webUrl = urllib.request.urlopen("\
@@ -14,21 +15,30 @@ Typos&action=raw")
     print("charset: ", webUrl.headers.get_param("charset"))
     print()
 
-    text = webUrl.read().decode('utf-8')
+    htmlText = webUrl.read().decode('utf-8')
     # print(text)
 
     typoRegex = regex.compile("\
 <(?:Typo)?\\s+(?:word=\"(.*?)\"\\s+)?find=\"(.*?)\"\\s+replace=\"(.*?)\
 \"\\s*/?>")
 
-    rules = regex.findall(typoRegex, text)
+    rules = regex.findall(typoRegex, htmlText)
+
+    fh = open(argv[1], 'r')
+    stext = fh.read()
+    fh.close()
+
     for rule in rules:
-        print("Rule Name = ", rule[0])
-        print("Rule Regex = ", rule[1])
-        print("Rule Substitution = ", rule[2])
-        print()
+        ruleRegex = regex.compile(rule[1])
+        for line in stext.splitlines():
+            if regex.search(ruleRegex, line):
+                print("line =", line)
+                print("Rule Name = ", rule[0])
+                print("Rule Regex = ", rule[1])
+                print("Rule Substitution = ", rule[2])
+                print()
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[0:])
 
 print("END")
